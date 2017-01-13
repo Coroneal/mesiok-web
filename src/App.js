d6e8ad13ba415988ddcaeecfed08042fb6259b35
coroneal <kulig.slawomir@gmail.com>
2017-01-14 00:25:02 +0100

    top bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppTopBar from './AppTopBar';
 
 const App = () => (
     <MuiThemeProvider>
-        <RaisedButton label="Default" />
+        <AppTopBar/>
+        {/*<RaisedButton label="Default" />*/}
     </MuiThemeProvider>
 );
 
